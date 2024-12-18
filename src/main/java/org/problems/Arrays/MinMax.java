ca6d6b3753760ba6fce8a70634264be033e73dc5
Firoz kumar <firozbhaikardar21@gmail.com>
2024-12-18 22:14:18 +0530

    added new problems

---
+++ src/main/java/org/problems/Arrays/MinMax.java
@@ -0,0 +1,17 @@
+package org.problems.Arrays;
+
+public class MinMax {
+
+
+    public static void main(String[] args) {
+        getMinMax(new int[]{1, 1, 3,4,5,100,0});
+    }
+
+    public static Pair<Long, Long>  getMinMax(int[] arr) {
+
+        Pair
+        for(int i=0;i<arr.length;i++) {
+
+        }
+    }
+}
---
+++ src/main/java/org/problems/Demo/Interview.java
@@ -0,0 +1,16 @@
+package org.problems.Demo;
+
+public class Interview {
+
+    public static void main(String[] args) {
+
+        test();
+
+    }
+
+    public static void test()
+    {
+
+        
+    }
+}
